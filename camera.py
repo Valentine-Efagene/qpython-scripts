8332c64e4bee51dd2a02c716ede0299a9ecb199f
Valentine Efagene <efagenevalentine@gmail.com>
2018-07-22 21:05:09 +0100

    update

--- camera.py
+++
@@ -1,6 +0,0 @@
-from androidhelper import Android
-
-droid = Android()
-path = "/sdcard/qpython/photos/test.jpg"
-droid.cameraCapturePicture(path)
-#droid.cameraInteractiveCapturePicture(path)
--- cvSaveImage.py
+++
@@ -1,8 +0,0 @@
-import cv
-
-#to use relative path, note that the working directory is the main qpython folder
-#to use absolute path, just copy and paste the path from your file manager 
-srcPath = 'great.jpg'
-desPath = 'call.jpg'
-img = cv.imread(srcPath, 0)
-cv.imwrite(desPath, img)
