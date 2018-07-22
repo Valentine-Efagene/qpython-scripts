e515eb5ae574801cef5f1c2234eefe3d0993e626
Valentine Efagene <efagenevalentine@gmail.com>
2018-07-22 21:07:56 +0100

    updae

--- matlibplotTest.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt 
-import numpy as np
-
-fig = plt.figure() 
-a = np.linspace(-np.pi, np.pi, 256)
-b = np.sin(a)
-plt.plot(a, b) 
-fig.savefig('temp.png', dpi=fig.dpi)
--- sl4aToCvCaptureImage.py
+++
@@ -1,9 +0,0 @@
-import cv
-from androidhelper import Android
-
-droid = Android()
-path = "/sdcard/qpython/photos/test.jpg"
-droid.cameraCapturePicture(path)
-#droid.cameraInteractiveCapturePicture(path)
-img = cv.imread(path)
-cv.imwrite('/sdcard/qpython/photos/cvImage.jpg', img)
