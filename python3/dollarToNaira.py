544ebdd5c6283fc30317a053704b9170464eaa32
Valentine Efagene <efagenevalentine@gmail.com>
2018-07-23 15:49:28 +0100

    update

--- python3/dollarToNaira.py
+++ python3/dollarToNaira.py
@@ -7,24 +7,27 @@ import re
 import androidhelper
 
 droid = androidhelper.Android()
-amountInDollars = float(droid.dialogGetInput('Username').result)
+amountInDollars = float(droid.dialogGetInput('Amount in dollars').result)
 fp = urllib.request.urlopen("https://www.xe.com/currencyconverter/convert/?Amount=1&From=USD&To=NGN") 
 mybytes = fp.read() 
 mystr = mybytes.decode("utf8") 
 fp.close() 
 
-if re.search("\d{3}.\d{3} NGN", mystr):
-  l_rate = re.findall("\d{3}.\d{3} NGN", mystr)
+if re.search("\d{2,}.\d{2,} NGN", mystr):
+  l_rate = re.findall("\d{2,}.\d{2,} NGN", mystr)
   s_rate = l_rate[0] 
   regex = re.compile(" NGN");
   s_rate = regex.sub("", s_rate);
   rate = float(s_rate)
   amountInNaira = rate * amountInDollars
-  print(rate)
-  print(amountInNaira)
-  title = str(amountInDollars) + " dollars in naira is";
-  message = str(amountInNaira)
+  d = "%.2f" %amountInDollars
+  n = "%.2f" %amountInNaira
+  print(d)
+  print(n)
+  title = "$" + d + "  in naira is";
+  message = "N" + n
   droid.dialogCreateAlert(title, message)
   droid.dialogShow()
 else:
-  print("found nothing")
+  print("ERROR:")
+  print("Could not obtain exchange rate.")
